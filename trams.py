4dac0fe4dc3c6f2d6aa4df40ade71c58c8e72b25
Ville Petersson <ville.petersson@gmail.com>
2013-06-03 16:39:50 +0200

    Made everything into functions.

--- trams.py
+++ trams.py
@@ -10,23 +10,28 @@ def colorize(fg,bg):
 
 endcolor = '\x1b[0m'
 
-page = urllib.urlopen("http://wap.vasttrafik.se/QueryForm.aspx?hpl=Godhemsgatan+(Göteborg)").read()
-soup = BeautifulSoup(page, convertEntities=BeautifulSoup.HTML_ENTITIES)
+def get_trams(stopname="Godhemsgatan+(Göteborg)"):
+	page = urllib.urlopen("http://wap.vasttrafik.se/QueryForm.aspx?hpl=%s"%(stopname)).read()
+	soup = BeautifulSoup(page, convertEntities=BeautifulSoup.HTML_ENTITIES)
 
-trams = list()
+	trams = list()
 
-# Find all trams in the list.
-for tr in soup.body.find('table', attrs={'id':'GridViewForecasts'}).findAll('tr'):
-	# Skip the header.
-	if tr['class']=="darkblue_pane":
-		continue
+	# Find all trams in the list.
+	for tr in soup.body.find('table', attrs={'id':'GridViewForecasts'}).findAll('tr'):
+		# Skip the header.
+		if tr['class']=="darkblue_pane":
+			continue
 
-	num = tr.findNext('td')
-	to = num.findNext('td')
-	next = to.findNext('td')
-	nextnext = next.findNext('td').findNext('td')
-	trams.append(dict(num=num.font.text,numbg=num['bgcolor'],numfg=num.font['color'],to=to.font.text,next=next.font.text.replace('Nu','0'),nextnext=nextnext.font.text))
+		num = tr.findNext('td')
+		to = num.findNext('td')
+		next = to.findNext('td')
+		nextnext = next.findNext('td').findNext('td')
+		trams.append(dict(num=num.font.text,numbg=num['bgcolor'],numfg=num.font['color'],to=to.font.text,next=next.font.text.replace('Nu','0'),nextnext=nextnext.font.text))
 
-for tram in sorted(trams, key=lambda tram: int(tram['next'])):
-	print colorize(tram['numfg'],tram['numbg'])+'\t'+tram['num']+'\t'+endcolor+'\t'+tram['to']+(20-len(tram['to']))*' '+'\t\x1b[1m'+tram['next']+'\x1b[0m\t'+tram['nextnext']
+	return trams
 
+def print_trams(trams):
+	for tram in sorted(trams, key=lambda tram: int(tram['next'])):
+		print colorize(tram['numfg'],tram['numbg'])+'\t'+tram['num']+'\t'+endcolor+'\t'+tram['to']+(20-len(tram['to']))*' '+'\t\x1b[1m'+tram['next']+'\x1b[0m\t'+tram['nextnext']
+
+#print_trams(get_trams())
